74d911e4d04c62754412c296f60682c3582a858e
abhij130403 <abhijeetojha2002@gmail.com>
2025-05-20 10:45:09 +0530

    Update DoubleLinkList.java

--- double_Link_List/DoubleLinkList.java
+++ double_Link_List/DoubleLinkList.java
@@ -1,4 +1,5 @@
 package double_Link_List;
+//Ques - creating Double linklist and there all operation
 class Node{
 	int no;
 	Node next;
@@ -26,6 +27,17 @@ class myDoubleLinkList{
 			temp.next = p;
 		}
 	}
+	//inserting the element at the begging of the linked list;
+	public void insertAtBeg(int no) {
+		Node p = new Node(no);
+		if(start == null) {
+			start = p;
+		} else {
+			start.prev = p;
+			p.next = start;
+			start = p;
+		}
+	}
 	public void traverse() {
 		if(start == null) {
 			System.out.println("Link list os Empty");
@@ -51,10 +63,13 @@ public class DoubleLinkList {
 	public static void main(String[] args) {
 		myDoubleLinkList p = new myDoubleLinkList();
 		p.insert(10);
-		p.insert(56);
+		p.insert(56); 
 		p.insert(46);
 		p.insert(43);
 		p.insert(34);
+		p.traverse();
+		p.insertAtBeg(23);
+		System.out.println("");
 		p.traverse();
 	}
 
